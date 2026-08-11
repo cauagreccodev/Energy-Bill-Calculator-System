8230527336176804d522a64ddedd46918ef8d724
cauagreccodev <cauagrecco11@gmail.com>
2026-08-10 22:56:05 -0300

    Preserve Prize-Table history into Main/Prize_Table.java and remove root duplicates

--- Client.java
+++
@@ -1,7 +0,0 @@
-import java.util.List;
-import java.util.ArrayList;
-
-public class Client {
-    List<Account> MyAccounts = new ArrayList<>();
-    
-}
--- Main.java
+++
@@ -1,9 +0,0 @@
-package exercise;
-import Account.java;
-import Prize-Table.java;
-import Client.java;
-
-
-public class Main {
-    
-}
